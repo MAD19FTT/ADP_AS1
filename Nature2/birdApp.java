56b3eb2d7a030eaa1b954d2dbc093697d2ddb68a
19FTT1066 <63836646+MAD19FTT@users.noreply.github.com>
2021-03-20 14:41:36 +0800

    Update birdApp.java
    Mouse motion to the bird

--- Nature2/birdApp.java
+++ Nature2/birdApp.java
@@ -1,12 +1,19 @@
 package Nature2;
 
+
+
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class birdApp extends Application{
+	double orgSceneX, orgSceneY;
 
 	@Override
 	public void start(Stage arg0) throws Exception {
@@ -17,12 +24,46 @@ public class birdApp extends Application{
 		
 		Background ground = new Background();
 		Bird bird = new Bird();
+		babyBird baby = new babyBird();
+		
+		bird.setOnMousePressed(e -> {
+		
+			
+			orgSceneX = e.getSceneX();
+			orgSceneY = e.getSceneY();
+			
+			Bird b = (Bird) e.getSource();
+			
+			b.toFront();
+		});
+		
+		bird.setOnMouseDragged(e -> {
+			double offsetX = e.getSceneX() - orgSceneX;
+			double offsetY = e.getSceneY() - orgSceneY;
+			
+			Bird b = (Bird) e.getSource();
+			
+			b.setLayoutX(b.getLayoutX() + offsetX);
+			b.setLayoutY(b.getLayoutY() + offsetY);
+			
+			orgSceneX = e.getSceneX();
+			orgSceneY = e.getSceneY();
+			
+		});
+		
+//		Timeline animation = new Timeline(new KeyFrame(Duration.millis(50), e->{}));
+//		animation.setCycleCount(Timeline.INDEFINITE);
+//		animation.play();
 		
 		Pane root = new Pane();
-		root.getChildren().addAll(ground, bird);
+		root.getChildren().addAll(ground, bird, baby);
 		
 		Scene scene = new Scene(root,1000,600);
 		scene.setFill(Color.LIGHTBLUE);
+		
+		scene.addEventFilter(MouseEvent.MOUSE_PRESSED, e-> {
+			bird.toFront();
+		});
 		
 		arg0.setScene(scene);
 		arg0.show();
@@ -33,4 +74,3 @@ public class birdApp extends Application{
 	}
 	
 }
-

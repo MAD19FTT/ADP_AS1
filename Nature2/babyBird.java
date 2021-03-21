48a66f27496399b46ae5871acc0406f2d6e5c727
19FTT1066 <63836646+MAD19FTT@users.noreply.github.com>
2021-03-22 02:29:35 +0800

    Add motion to baby beak

--- Nature2/babyBird.java
+++ Nature2/babyBird.java
@@ -6,34 +6,37 @@ import javafx.scene.shape.*;
 
 public class babyBird extends Pane{
 	
+	Polygon beak1, beak2;
+	boolean beakClose = false;
+	
 	public babyBird() {
 	Arc nest = new Arc(300, 140, 75, 75, 180 + 15, 150);
 	nest.setFill(Color.RED);
 	
 	
-	Circle baby = new Circle(300, 150, 20);
+	Circle baby = new Circle(300, 150, 15);
 	baby.setFill(Color.ORANGE);
 	baby.setStroke(Color.BLACK);
 	
-	Polygon beak1 = new Polygon();
+	beak1 = new Polygon();
 	beak1.getPoints().addAll(
-			300.0, 130.0,
-			310.0, 135.0,
-			330.0, 100.0
+			300.0, 135.0,
+			310.0, 140.0,
+			320.0, 120.0
 			);
 	beak1.setFill(Color.YELLOW);
 	beak1.setStroke(Color.BLACK);
 	
-	Polygon beak2 = new Polygon();
+	beak2 = new Polygon();
 	beak2.getPoints().addAll(
 			300.0, 150.0,
 			310.0, 135.0,
-			350.0, 125.0
+			330.0, 130.0
 			);
 	beak2.setFill(Color.YELLOW);
 	beak2.setStroke(Color.BLACK);
 	
-	Circle eye = new Circle(305,140,5);
+	Circle eye = new Circle(305, 140, 2);
 	
 	this.getChildren().addAll(
 			beak2,
@@ -44,4 +47,20 @@ public class babyBird extends Pane{
 		
 			);
 	}
+	
+	public void moveBeak() {
+		if(beakClose == false) {
+		
+			beak1.getPoints().remove(beak1.getPoints().size() -1);
+			beak1.getPoints().remove(beak1.getPoints().size() -1);
+			beak1.getPoints().addAll(330.0, 125.0);
+			beakClose = true;
+		} else {
+			beak1.getPoints().remove(beak1.getPoints().size() -1);
+			beak1.getPoints().remove(beak1.getPoints().size() -1);
+			beak1.getPoints().addAll(320.0, 120.0);
+			beakClose = false;
+		}
+	}
+	
 }

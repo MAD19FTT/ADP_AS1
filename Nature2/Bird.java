d28b99bac1e73c2574691f27bad960dd6f93e9e9
19FTT1066 <63836646+MAD19FTT@users.noreply.github.com>
2021-03-22 13:35:09 +0800

    move wing added

--- Nature2/Bird.java
+++ Nature2/Bird.java
@@ -1,53 +1,153 @@
-package Project01;
+package Nature2;
 
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Polygon;
+import javafx.scene.shape.*;
 
-public class Bird extends Pane{
-	Polygon beak, wing;
-	Circle body, eye;
+
+public class Bird extends Pane {
+	boolean wingUp = false;
+	Polygon wing1, wing2;
 	
 	public Bird() {
-		beak = new Polygon();
-		beak.getPoints().addAll(
-				35.0, 15.0,
-				50.0, 20.0,
-				35.0, 25.0
+		wing1 = new Polygon();
+		wing1.getPoints().addAll(
+				0.0, 22.5,
+				75.0, 22.5,
+				75.0, 75.0
 				);
-		beak.setFill(Color.YELLOW);
-		body = new Circle(20, 20, 20);
-		body.setFill(Color.BROWN);
-		eye = new Circle(30, 15, 5);
-		 eye.setFill(Color.BLACK);
-		 eye.setStroke(Color.WHITE);
-		 eye.setStrokeWidth(3);
-		 wing = new Polygon();
-		 wing.getPoints().addAll(
-		 10.0, 20.0,
-		 25.0, 25.0,
-		 15.0, 35.0
-		 );
-		 wing.setFill(Color.ORANGE);
-		 wing.setStroke(Color.BLACK);
-		 this.getChildren().addAll(beak, body, eye, wing);
+		wing1.setFill(Color.CYAN);
+		wing1.setStroke(Color.BLACK);
+		
+		wing2 = new Polygon();
+		wing2.getPoints().addAll(
+				150.0, 75.0,
+				75.0, 22.5,
+				75.0, 75.0
+				);
+		wing2.setFill(Color.CYAN);
+		wing2.setStroke(Color.BLACK);
+		
+		Polygon body = new Polygon();
+		body.getPoints().addAll(
+				75.0, 75.0,
+				150.0, 75.0,
+				75.0, 150.0
+				);
+		body.setFill(Color.CYAN);
+		body.setStroke(Color.BLACK);
+		
+		Polygon tail = new Polygon();
+		tail.getPoints().addAll(
+				75.0, 112.5,
+				112.5, 150.0,
+				75.0, 225.0
+				);
+		tail.setFill(Color.CYAN);
+		tail.setStroke(Color.BLACK);
+		
+		Polygon neck = new Polygon();
+		neck.getPoints().addAll(
+				129.0, 60.0,
+				150.0, 48.75,
+				150.0, 75.0
+				);
+		neck.setFill(Color.CYAN);
+		neck.setStroke(Color.BLACK);
+		
+		Polygon beak1 = new Polygon();
+		beak1.getPoints().addAll(
+				180.0, 63.75,
+				210.0, 86.25,
+				180.0, 75.0
+				);
+		beak1.setFill(Color.YELLOW);
+		beak1.setStroke(Color.BLACK);
+		
+		
+		Polygon beak2 = new Polygon();
+		beak2.getPoints().addAll(
+				168.75, 75.0,
+				180.0, 75.0,
+				191.25, 105.0
+				);
+		beak2.setFill(Color.YELLOW);
+		beak2.setStroke(Color.BLACK);
+		
+		Polygon head = new Polygon();
+		head.getPoints().addAll(
+				150.0, 48.75,
+				180.0, 48.75,
+				180.0, 75.0,
+				150.0, 75.0
+				);
+		head.setFill(Color.CYAN);
+		head.setStroke(Color.BLACK);
+		
+		Circle eye = new Circle(172.5,56.25,2.25);
+		eye.setFill(Color.BLACK);
+		
+		this.getChildren().addAll(
+				body,
+				tail,
+				neck,
+				beak1,
+				beak2,
+				head,
+				eye,
+				wing1,
+				wing2
+				);
+		this.setTranslateX(60);
+		this.setTranslateY(50);
+		this.setScaleX(1.25);
+		this.setScaleY(1.25);
+	}
+
+	public void flipRight() {
+		this.setScaleX(1.25);
 	}
 	
-	public void wingUp() {
-		wing.getPoints().remove(wing.getPoints().size()-1);
-		wing.getPoints().remove(wing.getPoints().size()-1);
-		wing.getPoints().addAll(
-				25.0, 10.0
-				);
+	public void flipLeft() {
+		this.setScaleX(-1.25);
+	
 	}
 	
-	public void wingDown() {
-		wing.getPoints().remove(wing.getPoints().size()-1);
-		wing.getPoints().remove(wing.getPoints().size()-1);
-		wing.getPoints().addAll(
-				15.0, 35.0
-				);
+	public void moveWing() {
+		if(wingUp == false) {
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing1.getPoints().addAll(70.0, 90.0, 85.0, 140.0, 0.0, 185.0);
+			wing2.getPoints().addAll(150.0, 75.0, 70.0, 90.5, 85.0, 140.0);
+			//System.out.println(wing1.getPoints().size()-1);
+			wingUp = true;
+		} else {
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing1.getPoints().remove(wing1.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing2.getPoints().remove(wing2.getPoints().size()-1);
+			wing1.getPoints().addAll(0.0, 22.5, 75.0, 22.5, 75.0, 75.0);
+			wing2.getPoints().addAll(150.0, 75.0, 75.0, 22.5, 75.0, 75.0);
+			wingUp = false;
+		}
 	}
 	
 }
